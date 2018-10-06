c3a225f140af7e13a434db548989208e0b377821
danko@Dmitry Tretyakov <d-tretyakov-artim2012@yandex.ru>
2018-10-06 10:25:41 +0300

    Lection 3

--- 3_10.c
+++ 3_10.c
@@ -1,37 +1,21 @@
 #include<stdio.h>
 
-main()
+int main()
 {
-    int x_10_start, x_2, i, x10_finish, a = 2;
-    printf("Введите числов десятичной системе счисления: "); scanf("%d", &x_10);
-    x_10_end = x_10;
-    x_10_start = x_10;
-    x_10_finish = 0;
-    system("clear");
-    printf("1");
-    while ()
-    {
-        while (x_10_finish != 1)
-        {
-            x_10_start = x_10_start / 2;
-            if (x_10_start == a) {
-                x_2 = x_10_start % 2;
-                if (x_10_start % 2 == 0)
-                    a *=2;
-                if (x_10_start % 2 == 1)
-                    a = (a * 2) + 1;
-                x_10_finish = 1;
-            }
-            if (x_10_start == b) {
-                x_2 = x_10_start % 2;
-                if 
-                b *=2;
-            }
-        }
-        if (x_2 == 0) printf("0"); 
-        if (x_2 == 1) printf("1");
-        x_10_finish = 0;
-        x_10_start = x_10;
-    }
-    
+     int fctr = 0, i, n;
+     printf("Введите число n: "); scanf("%d", &n);
+     system("clear");
+     i = n - 1;
+     while (i != 0)
+     {
+     	if (n - 1 == i) {
+     		fctr = n * (n-i);
+     		i = i - 1;
+     	} else {
+     		fctr = fctr * (n - i);
+     		i = i - 1;
+     	}
+     }
+     printf("Значение факториала равно: %d\n", fctr);
+     return 0;	
 }
