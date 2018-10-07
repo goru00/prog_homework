62b41e6afa2607d1dae8dd89691030aa0420e30b
danko@Dmitry Tretyakov <d-tretyakov-artim2012@yandex.ru>
2018-10-07 19:18:04 +0300

    Lection 3

--- 3_11.c
+++ 3_11.c
@@ -2,27 +2,26 @@
 
 main()
 {
-    float fctr, e = 1.0, fctr_finish, n, i;
+    float fctr, e = 1.0, n, i;
     printf("Введите число n: "); scanf("%f", &n);
     system("clear");
     fctr = n;
     i = n - 1.0;
-    fctr_finish = i;
     if (n == 1.0) e = e + 1.0;
     while (i != 0)
     {
-        while (fctr_finish != 0)
+        while (n != 0)
         {
-            fctr = fctr * fctr_finish;
-            fctr_finish -= 1.0;
+            fctr = fctr * n;
+            n = n - 1.0;
         }
         e = e + (1.0/fctr);
         i = i - 1.0;
-        fctr_finish = i;
-        n = n - 1.0;
-        fctr = n - 1.0;
+        n = i;
+        fctr = n + 1;
     }
-    printf("Выражение равно: %f\n", e);
+    e = e + 1;
+    printf("Выражение равно: %.3f\n", e);
 
     return 0;
 }
