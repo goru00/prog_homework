843ecfb797e9f3b1ea08feaa2bd6b031dfe84344
danko@Dmitry Tretyakov <d-tretyakov-artim2012@yandex.ru>
2018-10-08 20:37:35 +0300

    Lection 3

--- 3_3.c
+++ 3_3.c
@@ -6,7 +6,7 @@ int main()
 
     printf("Какова сторона квадрата?: "); scanf("%d", &x);
     system("clear");
-    while (i < x*x)
+    while (i <= x*x)
     {
         if (i % x == 0)
             printf("*\n");
--- 3_9.c
+++ 3_9.c
@@ -6,13 +6,14 @@ main()
     printf("Введите стороны прямоугольника: ");
     scanf("%d%d%d", &x1,&x2,&x3);
     system("clear");
-    if ((x1 == x2) && ((x1 != x3)) && (x2 != x3)) {
-        printf("Могут\n");
-    } else if ((x1 == x3) && ((x1 != x2)) && (x3 != x2)) { 
-        printf("Могут\n");
-    } else if ((x2 == x3) && ((x2 != x1)) && (x3 != x1)) {
-        printf("Могут\n");
-    } else printf("Не могут\n");
-
+    if ((x1 == 0) && ((x2 == 0)) && (x3 == 0)) {
+        if ((x1 == x2) && ((x1 != x3)) && (x2 != x3)) {
+            printf("Могут\n");
+        } else if ((x1 == x3) && ((x1 != x2)) && (x3 != x2)) { 
+            printf("Могут\n");
+        } else if ((x2 == x3) && ((x2 != x1)) && (x3 != x1)) {
+            printf("Могут\n");
+        } else printf("Не могут\n");
+    } else printf("Стороны не могут равняться 0!\n");
     return 0;
 }
