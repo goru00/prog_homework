4db93dfe6a778badeeaa643e4b5e3d63e3cfeff3
danko <dankadrx@yandex.ru>
2018-10-22 23:06:54 +0300

    Lection 4

--- 4_9.c
+++ 4_9.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-main()
+int main()
 {
-    int 
+    int i, num, num_quest;
+    printf("Отгадайте число от 1 до 100 с 3-ех попыток.\n");
+    srand(time(NULL));
+    num_quest = rand();
+    for (i = 3; i > 0; i--)
+    {
+        printf("Введите число: ");
+        scanf("%d", &num);
+        if (num == (num_quest % 100)) {
+            printf("Да, это число %d\n", num_quest % 100);
+            return 0;
+        }
+        else printf("Нет, это не %d. Попробуйте еще раз!(Осталось %d попытки(-а))\n", num, i - 1);
+    }
+    sleep(1);
+    system("clear");
+    if (i == 0) printf("3-и попытки кончились. Это было число %d. Пока.\n", num_quest % 100);
+    return 0;
 }
