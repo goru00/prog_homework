dd63a988e59558555a46ab752b5ced95d669edba
danko <d-tretyakov-artim2012@yandex.ru>
2019-03-09 14:20:10 +0300

    UPDATE

--- lection_8/8_3.c
+++ lection_8/8_3.c
@@ -3,20 +3,25 @@
 #include<time.h>
 void input();
 void output();
-int pop(int *, int);
-int push(int *, int);
+void pop();
+void push();
 void relocate();
 int main()
 {
 	int *array = NULL, n, d;
-	int (*a[2])(int *, int) = {push, pop};
 	printf("Введите кол-во книг в стопке: "); scanf("%d", &n);
 	array = (int *)malloc(n * sizeof(int));
 	input(array, n);
 	output(array, n);
-	printf("Выберете действие: (1 - взять. 2 - добавить) "); scanf("%d", &d);
-	n = relocate((*a[d - 1])(array, n), d);
-	output(array, n - 1);
+	int (*a[2])(int *, int) = {push, pop};
+	printf("Выберете действие: (1 - добавить. 2 - взять) "); scanf("%d", &d);
+	relocate((*a[d - 1])(array, n), d);
+	if (d == 2)
+		output(array, n - 1);
+	if (d == 1){
+		array[n] = n + 1;
+		output(array, n + 1);
+	}
 	free(array);
 	return 0;
 }
@@ -40,14 +45,11 @@ void relocate(int *mass, int count)
 		printf("Выбрано действие 'Добавить' \n");
 	} else printf("Выбрано действие 'Взять' \n");
 }
-int push(int *mass, int count)
+void push(int *mass, int count)
 {
 	mass = (int *)realloc(mass, (count + 1) * sizeof(int));
-	mass[count] = count;
-	return count + 1;
 }
-int pop(int *mass, int count)
+void pop(int *mass, int count)
 {
 	mass = (int *)realloc(mass, (count - 1) * sizeof(int));
-	return count - 1;
 }
