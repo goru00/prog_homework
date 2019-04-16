79955701e7b55c6ae22b6f5a7434a24351d11f52
danko <d-tretyakov-artim2012@yandex.ru>
2019-04-16 12:29:53 +0300

    UPDATE SOCKET

--- socket/socket_client.c
+++ socket/socket_client.c
@@ -1,26 +1,61 @@
 #include<stdio.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
 #include<stdlib.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include<netdb.h>
 
-int main()
+void error(const char *msg)
 {
-	int network_socket;
-	network_socket = socket(AF_INET, SOCK_STREAM, 0);
+	perror(msg);
+	exit(1);
+}
 
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
-	server_address.sin_addr.s_addr = INADDR_ANY;
-	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-	if (connection_status == -1)
-		printf("ERROR CONNECTION!\n");
-	char server_response[256];
-	recv(network_socket, &server_response, sizeof(server_response), 0);
+int main(int argc, char *argv[])
+{
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 
-	printf("SERVER SENT: %s\n", server_response);
+	char buffer[256];
+	if (argc < 3) {
+		printf("Usage %s hostname port\n", argv[0]);
+		exit(0);
+	}
+	portno = atoi(argv[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("Error OpenSock\n");
+	server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		printf("Error, no such host\n");
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *) server.h_addr, (char *) &serv_addr.sin_addr.s_addr, server.h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		error("Connect failed\n");
+	while (1)
+	{
+		bzero(buffer, 255);
+		fgets(buffer, 255, stdin);
+		n = write(sockfd, buffer, strlen(buffer));
+		if (n < 0)
+			error("Error write\n");
+		bzero(buffer, 255);
+		n = read(sockfd, buffer, 255);
+		if (n < 0)
+			error("Error read\n");
+		printf("Server: %s", buffer);
 
-	close(network_socket);
+		int i = strncmp("Buy", buffer, 3);
+		if (i == 0)
+			break;
+	}
+	close(sockfd);
 	return 0;
 }
