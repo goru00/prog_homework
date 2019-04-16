f56f042ef8d2933b826d4cdc6590c37b77a74bf7
danko <d-tretyakov-artim2012@yandex.ru>
2019-04-16 11:54:58 +0300

    UPDATE SOCKET

--- socket/socket_server.c
+++ socket/socket_server.c
@@ -7,13 +7,35 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 
-/*void error(const char *msg)
+void error(const char *msg)
 {
 	perror(msg);
 	exit(1);
-}*/
-int main()
+}
+int main(int argc, char *argv[])
 {
-	printf("Hello\n");
+	if (argc < 2) {
+		printf("PORT NOT PROVIDED\n");
+		exit(1);
+	}
+	int sockfd, newsockfd, portno, n;
+	char buffer[255];
+
+	struct sockaddr_in serv_addr, cli_addr;
+	socklen_t clilen;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		error("Error opening Socket");
+	}
+	bzero((char *), &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		error("Failed bind");	
 	return 0;
 }
