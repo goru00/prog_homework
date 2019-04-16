569ce9d4ba97553dfd9ac4469a426f8e5d060b5a
danko <d-tretyakov-artim2012@yandex.ru>
2019-04-16 10:34:43 +0300

    SOCKET INIT

--- socket/socket_client.c
+++ socket/socket_client.c
@@ -2,8 +2,8 @@
 #include<stdlib.h>
 #include<sys/types.h>
 #include<sys/socket.h>
-
 #include<netinet/in.h>
+#include<ncurses.h>
 
 int main()
 {
@@ -14,7 +14,14 @@ int main()
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(9002);
 	server_address.sin_addr.s_addr = INADDR_ANY;
-
 	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	if (connection_status == -1)
+		printf("ERROR CONNECTION!\n");
+	char server_response[256];
+	recv(network_socket, &server_response, sizeof(server_response), 0);
+
+	printf("SERVER SENT: %s\n", server_response);
+
+	close(sock)
 	return 0;
 }
