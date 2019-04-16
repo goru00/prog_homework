d450e727392ee53c2ea81377fd03724ac67aa61d
danko <d-tretyakov-artim2012@yandex.ru>
2019-04-16 10:41:36 +0300

    UPDATE

--- socket/socket_server.c
+++ socket/socket_server.c
@@ -12,7 +12,7 @@ int main()
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in server_address;
-	server_address.sin_family = AF.AF_INET;
+	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(9002);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 
