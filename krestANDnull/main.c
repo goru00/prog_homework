4c00f5b66ae3f6b645d49c874deac799ca976e76
danko <d-tretyakov-artim2012@yandex.ru>
2019-04-16 15:28:04 +0300

    CREATE GAME

--- krestANDnull/main.c
+++ krestANDnull/main.c
@@ -32,7 +32,50 @@ void area_game(int **area, int role)
 {
 	create_area_game(area);
 	if (role == 2) { // host
+		system("clear");
 		output_area_game(area);
+		if (argc < 2) {
+			printf("PORT NOT PROVIDED\n");
+			exit(1);
+		}
+		int sockfd, newsockfd, portno, n;
+		char buffer[255];
+		struct sockaddr_in serv_addr, cli_addr;
+		socklen_t clilen;
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sockfd < 0) {
+			error("Error opening Socket");
+		}
+		bzero((char *) &serv_addr, sizeof(serv_addr));
+		portno = atoi(argv[1]);
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_addr.s_addr = INADDR_ANY;
+		serv_addr.sin_port = htons(portno);
+		if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+			error("Failed bind");	
+		listen(sockfd, 5);
+		clilen = sizeof(cli_addr);
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0)
+			error("Error accept\n");
+		while(1)
+		{
+			bzero(buffer, 256);
+			n = read(newsockfd, buffer, 255);
+			if (n < 0)
+				error("Error read\n");
+			printf("Client: %s\n", buffer);
+			bzero(buffer, 255);
+			fgets(buffer, 255, stdin);
+			n = write(newsockfd, buffer, strlen(buffer));
+			if (n < 0)
+				error("Error write\n");
+			int i = strncmp("Buy", buffer, 3);
+			if (i == 0)
+				break;
+		}
+		close(newsockfd);
+		close(sockfd);
 	}
 	if (role == 1) { // client
 
@@ -66,10 +109,6 @@ void output_area_game(int **area)
 			if (area[i][j] == 0) {
 				printf(" ");
 			} else printf("%d", area[i][j]);
-			if (((i == 0) || (i == SIZE_N)) && (area[i][j] == 9))
-				printf("-");
-			if (((j == 0) || (j == SIZE_M)) && (area[i][j] == 9))
-				printf("|");
 		}
 		printf("\n");
 	}
