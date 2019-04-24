0dc5432132badfe7d3574f9f0fee7f22d804f0aa
danko <d-tretyakov-artim2012@yandex.ru>
2019-04-25 00:15:39 +0300

    CREATE LAB1

--- lab_1/var10.c
+++ lab_1/var10.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#include<time.h>
 double f(double x, const double eps)
 {
 	int n = 0;
-	double a = 1, s = 0; 
+	double a = 1, s = 0;
 	while (fabs(a) > eps)
 	{
 		s += a; n++; 
-		a *= - x * (2 * n - 1)/(2 * n);
+		a *= - x * (2 * n - 3)/(2 * n);
 	}
 	return s;
 }
 int main(int argc, char const *argv[])
 {
-	double x = 0.5;
+	double x = 0.7;
 	const double eps = 1e-16;
-	printf("%lf\t%lf\n", 1/sqrt(1 + x), f(x, eps));
+	printf("%lf\t%lf\n", sqrt(1 + x), f(x, eps));
 	return 0;
 }
