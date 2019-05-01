10021537160b04a011af3d04348cc882e2ffa4b5
danko <d-tretyakov-artim2012@yandex.ru>
2019-05-01 14:58:48 +0300

    UPDATE LAB_1

---
+++ lab_1/var3.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#include <math.h> 
+double F(double x, const double EPS) 
+{ 
+	double s=0, a=1,b; 
+	int n=0; 
+	x*=x; 
+	while(fabs(a)>EPS) 
+	{ 
+		b=2*a*x/((4*n+3)*(4*n+2)); 
+		s+=a+b; 
+		a=-2*b*x/((4*n+5)*(4*n+4)); 
+		n++; 
+	}  
+	return s*x; 
+} 
+int main() 
+{ 
+	const double EPS=1e-16; 
+	double x=1; 
+	printf("%lf\t%lf\n",cosh(x)*sin(x),F(x,EPS)); 
+	return 0; 
+}
