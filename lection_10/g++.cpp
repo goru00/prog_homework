c0ef88282c9b6b09a5cd9a0e5782db634d825d5d
student <student>
2019-05-10 09:54:36 +0300

    CREATE cpp

---
+++ lection_10/g++.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Введите целое число: "); scanf("%d", &a);
+	printf("Итак, Вы ввели число %d\n", a);
+	return 0;
+}
---
+++ lection_10/gcc.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout <<"Введите целое число: ";
+	cin >> a;
+	cout << "Итак, Вы ввели число " << a << endl;
+	return 0;
+}
