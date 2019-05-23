e95a6247d2774eaef39d874ec51847788e12e6d3
danko <d-tretyakov-artim2012@yandex.ru>
2019-05-24 00:12:44 +0300

    Chapter 1.2.1(Метод половинного деления)

---
+++ coursework/program_2_1_ur3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#define eps 0.00001
+double f(double,double,double);
+double FindRoot(double,double,double,double);
+double f(double x,double c,double d)
+{
+	return pow(x,5)+c*pow(x,2)-d;
+}
+double FindRoot(double a,double b,double c, double d)
+{
+	double cde; int i = 0;
+	while (b - a >= eps){
+        cde = (a + b) / 2;
+        if(f(b,c,d) * f(cde,c,d) < 0)
+            a = cde;
+        else
+            b = cde;
+    }
+    printf("Chislo iteracii: %d\n", i);
+    return cde;
+}
+int main()
+{
+	double a,b,c,d,x;
+	printf("Введите длину a и b: "); scanf("%lf%lf",&a,&b);
+	printf("Введите c и d: "); scanf("%lf%lf",&c,&d);
+	if(f(a,c,d)*f(b,c,d)>0)
+		printf("Ф-я не имеет корней на данном отрезке\n");
+	x = FindRoot(a,b,c,d);
+	printf("x = %lf, f(x) = %lf\n",x,f(x,c,d));
+	return 0;
+}
