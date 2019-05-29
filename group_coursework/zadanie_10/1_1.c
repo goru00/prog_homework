8b78d53761dfaf641491c81c21dd97fa917523c2
Дмитрий Третьяков <id140346916@dom.local>
2019-05-29 23:31:03 +0300

    UPDATE

--- group_coursework/zadanie_10/1_1.c
+++ group_coursework/zadanie_10/1_1.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 #include<math.h>
 #define PI 3.141592
-double f(double x, double eps)
+#define eps 1e-9
+double f(double x)
 {
 	int n = 1;
 	double s = 0, a = cos(n * x);
@@ -11,13 +12,14 @@ double f(double x, double eps)
 		s += pow(-1.0, n) * a; n++;
 		a = (cos(n * x))/(n*n);
 	}
+	printf("Кол-во слагаемых: %d\n", n);
 	return s;
 }
 int main()
 {
-	double eps, x = PI/5.0;
-	printf("%lf\n", x);
-	printf("Vvedite tochnost': "); scanf("%lf", &eps);
-	printf("%lf\t%lf\n", (1.0/4.0)*(x*x-(PI*PI)/3.0), f(x, eps));
+	double x;
+	printf("Введите значение x: "); scanf("%lf", &x);
+	if ((x < PI/5) || (x > PI)) printf("Неверные данные!\n");
+	else printf("%lf\t%lf\n", (1.0/4.0)*(x*x-(PI*PI)/3.0), f(x));
 	return 0;
 }
