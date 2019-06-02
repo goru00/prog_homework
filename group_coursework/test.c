95d54d1d40948c9e4e8b43e48f6942ea4d62ccc7
goru00 <d-tretyakov-artim2012@yandex.ru>
2019-06-02 17:45:16 +0300

    UPDATE

--- group_coursework/test.c
+++ group_coursework/test.c
@@ -9,10 +9,12 @@ void button()
     char select;
     #if _WIN32
         select = getch();
+        printf("WIN\n");
     #else
         system("stty raw");
         select = getchar();
         system("stty cooked");
+        printf("UNIX\n");
     #endif
     if (select == 56) printf("OK!\n");
 }
