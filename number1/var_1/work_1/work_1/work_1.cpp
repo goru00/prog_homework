8b4e6d4b351b600b9291ae0ab99278e0a0f146fc
Dmitrie Tretiakov <goru00@yandex.ru>
2020-01-25 19:27:37 +0400

    Variant 10, 1, 2

---
+++ number1/var_1/work_1/work_1/work_1.cpp
@@ -0,0 +1,70 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+class Angle
+{
+private:
+	double grad_angle, min_angle;
+public:
+	Angle(double m_grad_angle, double m_min_angle)
+	{
+		grad_angle = m_grad_angle;
+		min_angle = m_min_angle;
+	}
+	Angle() {} // настоящие мужики в любой ситуации создают конструктор по умолчанию
+	~Angle() {}
+	void rad()
+	{
+		grad_angle = grad_angle * 3.14159 / 180;
+		min_angle = min_angle * 3.14159 / (60 * 180);
+	}
+	void sum(double value)
+	{
+		grad_angle += value;
+		min_angle += value;
+	}
+	void substract(double value)
+	{
+		grad_angle -= value;
+		min_angle -= value;
+	}
+	void sinx() { sin(grad_angle); sin(min_angle); }
+	friend bool operator==(const Angle &angle1, const Angle &angle2);
+	friend bool operator<(const Angle& angle1, const Angle& angle2);
+	friend bool operator>(const Angle& angle1, const Angle& angle2);
+	friend ostream& operator<< (ostream& out, const Angle& angle);
+};
+bool operator==(const Angle &angle1, const Angle &angle2)
+{
+	return (angle1.grad_angle == angle2.grad_angle);
+}
+bool operator<(const Angle &angle1, const Angle &angle2)
+{
+	return (angle1.grad_angle < angle2.grad_angle);
+}
+bool operator>(const Angle &angle1, const Angle &angle2)
+{
+	return (angle1.grad_angle > angle2.grad_angle);
+}
+ostream& operator<< (ostream& out, const Angle& angle)
+{
+	out << angle.grad_angle << "\t" << angle.min_angle << endl;
+	return out;
+}
+int main()
+{
+	setlocale(0, "");
+	Angle obj1(135.5, 25);
+	Angle obj2(13, 23);
+	obj1.rad(); cout << "Переведем в радианы первый угол: " << obj1;
+	obj2.rad(); cout << "Переведем в радианы второй угол: " << obj2; // сравнивать в радианах два объекта как по мне намного легче :)
+
+	if (obj1 == obj2) cout << "Оба угла равны";
+	else if (obj1 > obj2) cout << "Первый угол больше второго\n";
+	else if (obj1 < obj2) cout << "Второй угол больше первого\n";
+
+	obj1.sum(156.5); cout << "Увеличение: " << obj1;
+	obj1.substract(123.7); cout << "Уменьшение: " << obj1;
+	obj1.sinx(); cout << "Преобразование в синус: " << obj1;
+	return 0;
+}
---
+++ number1/var_10/work_1/work_1/work_1.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+class Fraction
+{
+private:
+	int denominator, numerator;
+public:
+	Fraction(int m_numerator, int m_denominator) : numerator(m_numerator), denominator(m_denominator) { }
+	~Fraction() {}
+	double sum(Fraction &fraction2) { return (numerator / denominator + fraction2.numerator / fraction2.denominator); }
+	double substract(Fraction& fraction2) { return (numerator / denominator - fraction2.numerator / fraction2.denominator); }
+	double multiplication(Fraction& fraction2) { return (numerator / denominator * fraction2.numerator / fraction2.denominator); }
+	double division(Fraction& fraction2) { return ((numerator / denominator) / (fraction2.numerator / fraction2.denominator)); }
+	bool comparison(Fraction& fraction2) { if ((numerator / denominator) > (fraction2.numerator / fraction2.denominator)) return true; }
+	friend ostream& operator<<(ostream& out, const Fraction& fraction);
+	// написал просто для проверки равенства. но можно было все в метод один запихнуть, но я бака!
+	friend bool operator==(const Fraction& fraction1, const Fraction& fraction2);
+};
+ostream& operator<<(ostream& out, const Fraction& fraction)
+{
+	out << fraction.numerator << "/" << fraction.denominator;
+	return out;
+}
+bool operator==(const Fraction& fraction1, const Fraction& fraction2) { return ((fraction1.numerator / fraction1.denominator) == (fraction2.numerator / fraction2.denominator)); }
+int main()
+{
+	setlocale(0, "");
+	Fraction obj1(4, 2);
+	Fraction obj2(6, 3);
+	cout << "Сумма дроби " << obj1 << " и " << obj2 << " : " << obj1.sum(obj2) << endl;
+	cout << "Разность дроби " << obj1 << " и " << obj2 << " : " << obj1.substract(obj2) << endl;
+	cout << "Произведение дроби " << obj1 << " и " << obj2 << " : " << obj1.multiplication(obj2) << endl;
+	cout << "Деление дроби " << obj1 << " и " << obj2 << " : " << obj1.division(obj2) << endl;
+	cout << "Сравнение дроби " << obj1 << " и " << obj2 << " : ";
+	if (obj1 == obj2) cout << "Дроби равны" << endl; 
+	else if (!obj1.comparison(obj2))cout << "Дробь " << obj2 << " больше" << endl;
+	else if (obj1.comparison(obj2)) cout << "Дробь " << obj1 << " больше" << endl;
+	setlocale(0, "");
+	return 0;
+}
