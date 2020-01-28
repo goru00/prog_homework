13b866a55c970b9602b8bea43690dfac9aa93175
Dmitrie Tretiakov <goru00@yandex.ru>
2020-01-29 01:13:50 +0400

    Rewrite var_4 work_1

--- number1/var_4/work_1/work_1/work_1.cpp
+++ number1/var_4/work_1/work_1/work_1.cpp
@@ -3,20 +3,13 @@ using namespace std;
 class Matrix
 {
 private:
-	double **Matr;
+	double** Matr;
 	int n;
 	int m;
 public:
-    Matrix(int m_n, int m_m) : n(m_n), m(m_m) { Create(); }
+	Matrix() { Matr = 0; n = 0; m = 0; } // краткое пояснение. при создании матрицы без каких-либо значений, конструктор по умолчанию заполняет 0
+	Matrix(int m_n, int m_m) : n(m_n), m(m_m) { Create(); }
 	~Matrix() { }
-	void Delete()
-	{
-		for (int i = 0; i < m; i++)
-		{
-			delete[] Matr[i];
-		}
-		delete[] Matr;
-	}
 	void Create()
 	{
 		Matr = new double* [n];
@@ -29,50 +22,79 @@ public:
 	}
 	Matrix prfornum(int value)
 	{
-		Matrix temp(*this);
+		Matrix temp = Matrix(n, m);
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				temp.Matr[i][j] = Matr[i][j] * value;
+				temp[i][j] = Matr[i][j] * value;
 			}
 		}
-		return *this;
+		return temp;
 	}
-	Matrix pr(const Matrix &b)
+	Matrix pr(const Matrix& b)
 	{
-		Matrix temp(*this);
+		Matrix temp = Matrix(n, b.m);
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				temp.Matr[i][j] = Matr[i][j] * b.Matr[i][j];
+			for (int j = 0; j < b.m; j++) {
+				for (int k = 0; k < m; k++)
+					temp[i][j] += Matr[i][k] * b.Matr[k][j];
 			}
 		}
-		return *this;
+		return temp;
 	}
-	Matrix sum(const Matrix &b)
+	Matrix sum(const Matrix& b)
 	{
-		Matrix temp(*this);
+		Matrix temp = Matrix(n, b.m);
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				temp.Matr[i][j] = Matr[i][j] + b.Matr[i][j];
+			for (int j = 0; j < b.m; j++) {
+				temp[i][j] = Matr[i][j] + b.Matr[i][j];
 			}
 		}
-		return *this;
+		return temp;
 	}
-	Matrix substract(const Matrix &b)
+	Matrix substract(const Matrix& b)
 	{
-		Matrix temp(*this);
+		Matrix temp = Matrix(n, b.m);
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				temp.Matr[i][j] = Matr[i][j] - b.Matr[i][j];
+			for (int j = 0; j < b.m; j++) {
+				temp[i][j] = Matr[i][j] - b.Matr[i][j];
 			}
 		}
-		return *this;
+		return temp;
+	}/*
+	Matrix degree(int value)
+	{
+		double Q;
+		Matrix temp = Matrix(n, m);
+		for (int g = 0; g < value; g++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					for (int k = 0; k < m; k++)
+						Q += temp[i][k] * temp[k][j];
+					temp[i][j] += Q;
+				}
+			}
+			
+		}
+		return temp;
+	}*/
+	double* operator [] (int index)
+	{
+		return getRow(index);
+	}
+	double* getRow(int index)
+	{
+		if (index >= 0 && index < n)
+			return Matr[index];
+		return 0;
 	}
 	friend ostream& operator<<(ostream& out, const Matrix& matr);
 	friend bool operator==(const Matrix& matr1, const Matrix& matr2);
-	friend bool operator!=(const Matrix& matr1, const Matrix& matr2);
 };
-bool operator==(const Matrix& matr1, const Matrix& matr2) {	return (matr1.n == matr2.n && matr1.m == matr2.m);}
-bool operator!=(const Matrix& matr1, const Matrix& matr2) { return (matr1.n != matr2.m); }
+bool operator==(const Matrix& matr1, const Matrix& matr2)
+{
+	if (matr1.n != matr2.n || matr1.m != matr2.m)
+		return false;
+}
 ostream& operator<<(ostream& out, const Matrix& matr)
 {
 	for (int i = 0; i < matr.n; i++) {
@@ -86,14 +108,15 @@ ostream& operator<<(ostream& out, const Matrix& matr)
 int main()
 {
 	setlocale(0, "");
-	Matrix matr1(5, 4);
+	Matrix matr1(5, 5);
 	Matrix matr2(5, 5);
-	//matr1.display();
 	cout << "Первая матрица:\n" << matr1 << endl << "Вторая матрица:\n" << matr2 << endl;
-	if (matr1 == matr2) cout << "Сложение матриц:\n" << matr1.sum(matr2) << endl << "Вычитание матриц:\n" << matr1.substract(matr2) << endl;
-	else cout << "Матрицы имеют не равные размерности!\n";
-	/*if (!(matr1 != matr2)) cout << "Умножение матриц:\n" << matr1.pr(matr2);
-	else cout << "Матрицы имеют не равные размерности!\n";*/
+	if (matr1 == matr2) {
+		cout << "Сложение матриц:\n" << matr1.sum(matr2) << endl;
+		cout << "Разность матриц:\n" << matr1.substract(matr2) << endl;
+		cout << "Умножение матриц:\n" << matr1.pr(matr2);
+	}
 	cout << "Умножение матрицы 1 на число:\n" << matr1.prfornum(5) << endl;
+	//cout << "Возведение матрицы в степень:\n" << matr1.degree(3) << endl;
 	return 0;
 }
