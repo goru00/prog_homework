bae15c143d52a5b296c1fa82a2862dd0f9ff30b0
Dmitrie Tretiakov <goru00@yandex.ru>
2020-01-30 16:53:27 +0400

    Completed var_10 work_1

--- number1/var_10/work_1/work_1/work_1.cpp
+++ number1/var_10/work_1/work_1/work_1.cpp
@@ -1,40 +1,68 @@
 ﻿#include<iostream>
+#include<cassert>
 using namespace std;
 class Fraction
 {
 private:
 	int denominator, numerator;
 public:
-	Fraction(int m_numerator, int m_denominator) : numerator(m_numerator), denominator(m_denominator) { }
-	~Fraction() {}
-	double sum(Fraction &fraction2) { return (numerator / denominator + fraction2.numerator / fraction2.denominator); }
-	double substract(Fraction& fraction2) { return (numerator / denominator - fraction2.numerator / fraction2.denominator); }
-	double multiplication(Fraction& fraction2) { return (numerator / denominator * fraction2.numerator / fraction2.denominator); }
-	double division(Fraction& fraction2) { return ((numerator / denominator) / (fraction2.numerator / fraction2.denominator)); }
-	bool comparison(Fraction& fraction2) { if ((numerator / denominator) > (fraction2.numerator / fraction2.denominator)) return true; }
-	friend ostream& operator<<(ostream& out, const Fraction& fraction);
+	Fraction() { }
+	Fraction(int, int);
+	~Fraction() { }
+	double sum(Fraction&);
+	double substract(Fraction&);
+	double multiplication(Fraction&);
+	double division(Fraction&);
+	bool comparison(Fraction&);
+	friend ostream& operator<<(ostream&, const Fraction&);
 	// написал просто для проверки равенства. но можно было все в метод один запихнуть, но я бака!
-	friend bool operator==(const Fraction& fraction1, const Fraction& fraction2);
+	friend bool operator==(const Fraction&, const Fraction&);
 };
+Fraction::Fraction(int m_numerator, int m_denominator) 
+{
+	numerator = m_numerator;
+	denominator = m_denominator;
+}
+double Fraction::sum(Fraction& fraction2) 
+{ 
+	return (numerator / denominator + fraction2.numerator / fraction2.denominator); 
+}
+double Fraction::substract(Fraction& fraction2) 
+{ 
+	return (numerator / denominator - fraction2.numerator / fraction2.denominator); 
+}
+bool Fraction::comparison(Fraction& fraction2) 
+{ 
+	if ((numerator / denominator) > (fraction2.numerator / fraction2.denominator)) return true; 
+}
+double Fraction::division(Fraction& fraction2) { 
+	return ((numerator / denominator) / (fraction2.numerator / fraction2.denominator)); 
+}
+double Fraction::multiplication(Fraction& fraction2) 
+{ 
+	return (numerator / denominator * fraction2.numerator / fraction2.denominator); 
+}
 ostream& operator<<(ostream& out, const Fraction& fraction)
 {
 	out << fraction.numerator << "/" << fraction.denominator;
 	return out;
 }
-bool operator==(const Fraction& fraction1, const Fraction& fraction2) { return ((fraction1.numerator / fraction1.denominator) == (fraction2.numerator / fraction2.denominator)); }
+bool operator==(const Fraction& fraction1, const Fraction& fraction2) 
+{ 
+	return ((fraction1.numerator / fraction1.denominator) == (fraction2.numerator / fraction2.denominator)); 
+}
 int main()
 {
 	setlocale(0, "");
-	Fraction obj1(4, 2);
-	Fraction obj2(6, 3);
-	cout << "Сумма дроби " << obj1 << " и " << obj2 << " : " << obj1.sum(obj2) << endl;
-	cout << "Разность дроби " << obj1 << " и " << obj2 << " : " << obj1.substract(obj2) << endl;
-	cout << "Произведение дроби " << obj1 << " и " << obj2 << " : " << obj1.multiplication(obj2) << endl;
-	cout << "Деление дроби " << obj1 << " и " << obj2 << " : " << obj1.division(obj2) << endl;
-	cout << "Сравнение дроби " << obj1 << " и " << obj2 << " : ";
-	if (obj1 == obj2) cout << "Дроби равны" << endl; 
-	else if (!obj1.comparison(obj2))cout << "Дробь " << obj2 << " больше" << endl;
-	else if (obj1.comparison(obj2)) cout << "Дробь " << obj1 << " больше" << endl;
+	Fraction Obj1(4, 2), Obj2(6, 3);
+	cout << "Сумма дроби " << Obj1 << " и " << Obj2 << " : " << Obj1.sum(Obj2) << endl;
+	cout << "Разность дроби " << Obj1 << " и " << Obj2 << " : " << Obj1.substract(Obj2) << endl;
+	cout << "Произведение дроби " << Obj1 << " и " << Obj2 << " : " << Obj1.multiplication(Obj2) << endl;
+	cout << "Деление дроби " << Obj1 << " и " << Obj2 << " : " << Obj1.division(Obj2) << endl;
+	cout << "Сравнение дроби " << Obj1 << " и " << Obj2 << " : ";
+	if (Obj1 == Obj2) cout << "Дроби равны" << endl; 
+	else if (!Obj1.comparison(Obj2))cout << "Дробь " << Obj2 << " больше" << endl;
+	else if (Obj1.comparison(Obj2)) cout << "Дробь " << Obj1 << " больше" << endl;
 	setlocale(0, "");
 	return 0;
 }
