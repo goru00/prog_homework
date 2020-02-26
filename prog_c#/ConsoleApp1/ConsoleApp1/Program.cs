d7c70d9acd3cb5cb7f4de9d1264713283e15285a
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-02-26 10:46:06 +0000

    Created lab_5

---
+++ prog_c#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {     
+        static int[] pushMass(int[] mass)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < mass.Length; i++)                      
+                mass[i] = rand.Next(-10, 10);            
+            return mass;
+        }        
+        static void outMass(int[] mass)
+        {
+            for (int i = 0; i < mass.Length; i++)
+                System.Console.Write("\t" + mass[i]);
+            System.Console.WriteLine();
+        }
+        /*static int sumMass(int[] mass)
+        {
+            int sum = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+
+                sum += mass[i];
+            }
+            return sum;
+        }*/
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            const int n = 5;
+            int[] mass = new int[n];
+            pushMass(mass);
+            System.Console.WriteLine("Исходный массив:");
+            outMass(mass);
+            Array.Sort(mass);
+            outMass(mass);
+            //System.Console.WriteLine("Сумма элементов: ", sumMass(mass));
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ prog_c#/massive_1/massive_1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("massive_1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("massive_1")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("71aae581-6c38-4ce2-b399-24e2b269f8da")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
