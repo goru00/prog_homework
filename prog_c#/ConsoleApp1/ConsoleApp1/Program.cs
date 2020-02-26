3a6c87fdc4d354f97be06cd862f6eeb5ddb66cc0
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-02-26 12:08:51 +0000

    Completed lab_5

--- prog_c#/ConsoleApp1/ConsoleApp1/Program.cs
+++ prog_c#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -21,27 +21,38 @@ namespace ConsoleApp1
                 System.Console.Write("\t" + mass[i]);
             System.Console.WriteLine();
         }
-        /*static int sumMass(int[] mass)
+        static int sumMass(int[] mass)
         {
             int sum = 0;
             for (int i = 0; i < mass.Length; i++)
             {
-
-                sum += mass[i];
+                if (mass[i] > 0)
+                    sum += mass[i];
             }
             return sum;
-        }*/
+        }
+        static int countS(int[] mass)
+        {
+            int k = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] > 0) k++;             
+            }
+            return k;
+        }
         static void Main(string[] args)
         {
             Random rand = new Random();
             const int n = 5;
             int[] mass = new int[n];
             pushMass(mass);
-            System.Console.WriteLine("Исходный массив:");
+            System.Console.WriteLine("Исходный массив: ");
             outMass(mass);
-            Array.Sort(mass);
+            mass[rand.Next(0, n)] = 0;
+            Array.Sort(mass); // сортируем элементы 
             outMass(mass);
-            //System.Console.WriteLine("Сумма элементов: ", sumMass(mass));
+            System.Console.WriteLine("Кол-во положительных элементов: " + countS(mass));
+            System.Console.WriteLine("Сумма элементов: " + sumMass(mass));
             System.Console.ReadKey();
         }
     }
