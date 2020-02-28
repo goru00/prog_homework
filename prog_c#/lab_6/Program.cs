ab0779e10f3949908fd91c0e37cdb1ceb9748621
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-02-28 20:37:48 +0400

    Completed lab6

--- prog_c#/lab_6/Program.cs
+++ prog_c#/lab_6/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp1
             {
                 for (int j = 0; j < m; j++)
                 {
-                    mass[i, j] = rand.Next(-10, 10);
+                    mass[i, j] = rand.Next(-10, 5);
                 }
             }
             return mass;
@@ -30,13 +30,37 @@ namespace ConsoleApp1
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
         }
         static int[,] massSort(int[,] mass, int n, int m)
         {
-            int flag;
-            int k = 0, _i = 0, _j = 0;
-            for (int i = 0; i < n; )
+            int k = 0, _j = 0, flag, max = 0, countS = 0;
+            bool flagS = false;
+            for (int i = 0; i < n; i++, k++)
+            {
+                max = mass[k, k];
+                for (int j = 0; j < m; j++)
+                {
+                    if (mass[i, j] < 0) countS++;
+                    if (max <= mass[i, j])
+                    {
+                        max = mass[i, j];
+                        _j = j;
+                    }
+                }
+                if (countS == m && !flagS)
+                {
+                    itt(i);
+                    flagS = true;
+                }
+                countS = 0;
+                flag = mass[k, k];
+                mass[k, k] = max;
+                mass[i, _j] = flag;
+            }
+            return mass;
         }
+        static void itt(int count) { Console.WriteLine("Все отрицательные числа в строке номер: " + (count + 1)); }
         static void Main()
         {
             const int n = 5, m = 5;
@@ -44,6 +68,7 @@ namespace ConsoleApp1
             push(mass, n, m);
             output(mass, n, m);
             massSort(mass, n, m);
+            output(mass, n, m);
             Console.ReadKey();
         }
     }
