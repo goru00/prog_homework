523a2bb32d607387deaf1b4d362e0ba7905d7ab4
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-03-04 01:12:28 +0400

    Completed lab_1

---
+++ prog_c#/lab_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double z1, z2;
+            Console.Write("Введите значение m: ");
+            double m = Convert.ToDouble(Console.ReadLine());
+            z1 = Math.Sqrt(Math.Pow(3.0 * m + 2.0, 2.0) - 24.0 * m) / (3.0 * Math.Sqrt(m) - 2.0 / Math.Sqrt(m));
+            Console.WriteLine(z1);
+            z2 = -Math.Sqrt(m);
+            Console.WriteLine(z2);
+            Console.ReadKey();
+        }
+    }
+}
