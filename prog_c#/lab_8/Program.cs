4a322ef56616e1ba881986e12f2d08596e6aeee0
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-03-07 02:06:02 +0400

    Created lab_8

---
+++ prog_c#/lab_8/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Product
+    {
+        public string Name_product { get; private set; }
+        public string Name_store { get; private set; }
+        public decimal Price { get; private set; }
+        public Product(string name, string store, decimal price)
+        {
+            Name_product = name;
+            Name_store = store;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"Наименование товара: {Name_product}; Магазин: {Name_store}; Цена: {Price}";
+        }
+    }
+    class Store
+    {
+        Product[] data;
+        public int Lenght { get; private set; }
+        public Store(int length)
+        {
+            Lenght = length;
+            data = new Product[Lenght]; 
+        }
+        public Product this[int index]
+        {
+            get // вообще здесь хорошо сделать проверку через Exception. может сделаю, если понадобится
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+        public Product Find(string name)
+        {
+            foreach (Product element in data)
+            {
+                if (element.Name_product == name)
+                    return element;
+            }
+            return null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store product = new Store(2);
+            product[0] = new Product ("Колбаса Вязанка", "Пятерочка", 110);
+            product[1] = new Product("Колбаса Вязанка", "Пятерочка", 105);
+            Console.Write("Введите название товара: ");
+            string name_product = Console.ReadLine();
+            Product find = product.Find(name_product);
+            if (find != null) Console.WriteLine(find.ToString());
+            Console.ReadKey();
+        }
+    }
+}
