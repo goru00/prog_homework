1a96b67992aec0063599476ba9c7a02ff6f9772b
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-03-11 11:20:51 +0000

    Completed lab_2 work_2

---
+++ prog_c#/lab_2/work_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, y = 0;
+            int r = 0;
+            try
+            {
+                Console.Write("Введите x: ");
+                x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите y: ");
+                y = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите r: ");
+                r = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            if (Math.Sqrt(x * x + y * y) <= r)
+            {
+                if ((y >= 0 && y >= x) || (y <= 0 && y <= x))
+                    Console.WriteLine("Точка попадает в закр.область");
+                else Console.WriteLine("Точка НЕ попадает в закр.область");
+            }
+            Console.ReadKey();
+        }
+    }
+}
