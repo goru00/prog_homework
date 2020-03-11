c1ff6aa42add787601950081718cbf2f70f9f590
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-03-11 11:19:53 +0000

    Completed lab_3 work_2

---
+++ prog_c#/lab_3/work_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Shot(double x, double y, int r)
+        {
+            if (Math.Sqrt(x * x + y * y) <= r)
+            {
+                if ((y >= 0 && y >= x) || (y <= 0 && y <= x))
+                    Console.WriteLine("Точка попадает в закр.область");
+                else Console.WriteLine("Точка НЕ попадает в закр.область");
+            }
+        }
+        static void Main(string[] args)
+        {
+            double x, y;
+            int r;
+            Console.Write("Введите радиус: ");
+            r = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Введите координаты для {i + 1} мишени:\nx= ");
+                x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("y= ");
+                y = Convert.ToDouble(Console.ReadLine());
+                Shot(x, y, r);
+            }
+        }
+    }
+}
