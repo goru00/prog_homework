3c88464518f8d6554a3bf13fa5acd9b20120d893
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-04-03 16:39:52 +0400

    Create lab_10

---
+++ prog_c#/lab_10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public struct ZNAK
+    {
+        public string first_name;
+        public string second_name;
+        public string zodiac;
+        public int[] date;
+        public ZNAK(string first_name, string second_name, string zodiac, int[] date)
+        {
+            this.first_name = first_name;
+            this.second_name = second_name;
+            this.zodiac = zodiac;
+            this.date = new int[date.Length];
+            this.date = date;
+        }
+        public override string ToString()
+        {
+            return $"Имя: {first_name}; Фамилия: {second_name}; Зодиак: {zodiac}; Дата рождения: {date}";
+        }
+    }
+}
