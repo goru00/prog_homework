c7b9f1e935d7d9ec5c668444c7c9525d0d87b94b
Dmitrie Tretiakov <d-tretyakov-artim2012@yandex.ru>
2020-07-01 14:50:51 +0300

    Completed lab_2

--- prog_c#/lab_2/work_1/Program.cs
+++ prog_c#/lab_2/work_1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,48 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double y = 0, x = 0;
-            int r = 0;
-            try
-            {
-                Console.Write("Введите x: ");
-                x = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Введите r: ");
-                r = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (Exception error)
-            {
-                Console.WriteLine(error.Message);
-            }
-            int px1 = -5;
-            int px3 = -r - 2;
-            int px2 = px3 - r;
-            int px4 = 2;
-            int px5 = 4;
-            if (x < px1 || x > px5) throw new Exception("неверный задан диапазон");
-            if (px2 == x)
-            {
-                y = 0;
-            }
-            else if (px2 > x && px1 < x)
-            {
+            double x, y=-100;
+            double r;
+            Console.WriteLine("Введите R");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Х");
+            x = Convert.ToDouble(Console.ReadLine());
+            if (x < -5 || x > 5)
+                Console.WriteLine("Значение функции не определено");
+            else if (x >= -5 && x < -3)
                 y = 1;
-            }
-            else if (px1 < x && px2 > x)
-            {
-                y = Math.Sqrt(Math.Pow(r, 2) - Math.Pow(x - px2, 2));
-                y *= -1;
-            }
-            else if (px3 <= x && x <= px4)
-            {
+            else if (x == -3)
+                Console.WriteLine("В точке -3 функция имеет разрыв и принимает два значения у1=1 у2=0");
+            else if (x > -3 && x <= -1)
+                y = -Math.Sqrt(-Math.Pow(x + 1, 2) + r * r);
+            else if (x > -1 && x <= 2)
                 y = -2;
-            }
-            else if (px4 < x && px5 > x)
-            {
-                y = ((x - px4) * 2) / (px5 - px4);
-            }
-            Console.WriteLine("y = " + y);
+            else if (x > 2)
+                y = x - 4;
+            if (y != -100)
+                Console.WriteLine("Значение функции при Х={0:f2} равнo {1:f2}",x,y);
             Console.ReadKey();
+
         }
     }
 }
