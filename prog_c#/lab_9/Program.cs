490c03053f16ade40b51103de9e0a9aaceb5488b
danko <d-tretyakov-artim2012@yandex.ru>
2020-07-01 15:01:15 +0300

    Completed lab_9

--- prog_c#/lab_9/Program.cs
+++ prog_c#/lab_9/Program.cs
@@ -1,77 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Diagnostics;
-namespace ConsoleApp1
+
+namespace ConsoleApp7
 {
-    class Point
-    {
-        public double PointX { get; set; }
-        public double PointY { get; set; }
-        public Point() { }
-        public Point(double x, double y)
-        {
-            PointX = x;
-            PointY = y;
-        }
-        public virtual void Display()
-        {
-            Console.WriteLine($"F(x, y) = ({PointX};{PointY})");
-        }
-    }
-    class ColoredPoint : Point
-    {
-        public string Color { get; set; }
-        public ColoredPoint(double x, double y, string color) : base(x, y)
-        {
-            Color = color;
-        }
-        public override void Display()
-        {
-            Console.WriteLine($"F(x, y) = ({PointX};{PointY}); Color: {Color}");
-        }
-    }
-    class Line : Point
-    {
-        public double X_end { get; set; }
-        public double Y_end { get; set; }
-        public Line(double x, double y, double x_end, double y_end) : base(x, y)
-        {
-            X_end = x_end;
-            Y_end = y_end;
-        }
-        public override void Display()
-        {
-            Console.WriteLine($"F(x, y) = ({PointX};{PointY});");
-            Console.WriteLine($"F_end(x, y) = ({X_end};{Y_end});");
-        }
-    }
-    class ColoredLine : Line
-    {
-        public string Color { get; set; }
-        public ColoredLine(double x, double y, double x_end, double y_end, string color) : base(x, y, x_end, y_end)
-        {
-            Color = color;
-        }
-        public override void Display()
-        {
-            Console.WriteLine($"F(x, y) = ({PointX};{PointY});");
-            Console.WriteLine($"F_end(x, y) = ({X_end};{Y_end}); Color: {Color}");
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Point A = new Point(5, 5);
-            Point B = new Point(); 
-            Point AColor = new ColoredPoint(A.PointX, A.PointY, "Black");
-            Point AB = new Line(A.PointX, A.PointY, B.PointX, B.PointY);
-            Point ABColor = new ColoredLine(A.PointX, A.PointY, B.PointX, B.PointY, "Red");
+            var num1 = new DecimalStr(32);
+            Console.WriteLine($"Первое число: {num1.word}");
+            var num2 = new DecimalStr(12);
+            Console.WriteLine($"Второе число: {num2.word}");
+            DecimalStr res = num1 - num2;
+            Console.WriteLine($"Разность: {res.word}");
+            var word1 = new Str("Привет");
+            Console.WriteLine($"Длина строки: {word1.get_length()}");
+            word1.clear();
+            if (word1.get_length() == 0) Console.WriteLine("Строка пуста");
+            else Console.WriteLine("Строка не пуста");
+            Console.ReadKey();
         }
     }
+    class Str
+    {
+        private long length;
+        private string str;
+        public Str() { }
+        /* конструктор с параметрами, если получаем на вход строку */
+        public Str(string str)
+        {
+            this.str = str;
+            length = (long)str.Length;
+        }
+        /* если получаем на вход символ*/
+        public Str(char h)
+        {
+            str = Convert.ToString(h);
+            length = 1;
+        }
+        public void clear()
+        {
+            str = "";
+            length = 0;
+        }
+        public long get_length() => length;
+    }
+    class DecimalStr : Str
+    {
+        public string word { get; set; }
+        public DecimalStr(int a)
+        {
+            var new_str = new StringBuilder();
+            if (a < 0)
+            {
+                new_str.Append('-');
+            }
+            a = Math.Abs(a);
+            while (a > 0)
+            {
+                int temp = a % 10;
+                new_str.Insert(0, temp.ToString());
+                a /= 10;
+            }
+            word = new_str.ToString();
+        }
+        public static DecimalStr operator-(DecimalStr arg1, DecimalStr arg2)
+        {
+            int Out = Int32.Parse(arg1.word) - Int32.Parse(arg2.word);
+            return new DecimalStr(Out);
+        }
+        public static bool operator<(DecimalStr arg1, DecimalStr arg2) => (Int32.Parse(arg1.word) < Int32.Parse(arg2.word));
+        public static bool operator>(DecimalStr arg1, DecimalStr arg2) => (Int32.Parse(arg1.word) > Int32.Parse(arg2.word));
+    }
 }
-
