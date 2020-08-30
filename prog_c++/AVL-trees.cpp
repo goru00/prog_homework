90835108221a97cb2ae6370df7381f91f2b74a42
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-08-30 23:40:27 +0300

    Created Delete, Search

--- prog_c++/AVL-trees.cpp
+++ prog_c++/AVL-trees.cpp
@@ -81,6 +81,71 @@ public:
 		n->height = max(Height(n->left), Height(n->right)) + 1;
 		return n;
 	}
+	bool Delete(T data)
+	{
+		bool flag = false; // проверка на удаление
+		root = Delete(flag, data, root);
+		return flag;
+	}
+	Node<T>* Delete(bool& flag, T data, Node<T>* n)
+	{
+		if (n == NULL) return NULL;
+		else
+		{
+			if (data == n->data)
+			{
+				if (n->right == NULL)
+				{
+					Node<T>* temp = n;
+					n = n->left;
+					delete temp;
+					flag = true;
+					return n;
+				}
+				else
+				{
+					Node<T>* temp = n->right;
+					while (temp->left != NULL) temp = temp->left;
+					n->data = temp->data;
+					n->right = Delete(flag, temp->data, n->right);
+				}
+			}
+			else if (data < n->data) n->left = Delete(flag, data, n->left);
+			else n->right = Delete(flag, data, n->right);
+			if (Height(n->left) - Height(n->right) == 2)
+			{
+				if (data < n->left->data) n = RightRotate(n);
+				else n = LeftRightRotate(n);
+			}
+			else if (Height(n->left) - Height(n->right) == -2)
+			{
+				if (data > n->right->data) n = LeftRotate(n);
+				else n = RightLeftRotate(n);
+			}
+			n->height = max(Height(n->left), Height(n->right)) + 1;
+		}
+		return n;
+	}
+	bool Search(T data)
+	{
+		return (Search(data, root) == NULL) ? false : true;
+	}
+	Node<T>* Search(T data, Node<T>* n)
+	{
+		if (n == NULL) return NULL;
+		else if (data == n->data) return n;
+		else if (data < n->data) return Search(data, n->left);
+		return Search(data, n->right);
+	}
+	bool Update(T n_data, T o_data)
+	{
+		if (Delete(o_data))
+		{
+			Insert(n_data);
+			return true;
+		}
+		return false;
+	}
 	void Show()
 	{
 		Show(root);
@@ -133,7 +198,9 @@ public:
 };
 int main()
 {
+	setlocale(0, "");
 	const int n = 5;
+	// здесь side нужен был для тестирования правильности работы алгоритма
 	Data tab[] = {
 		{10, "left"},
 		{5, "middle"},
@@ -148,5 +215,27 @@ int main()
 	}
 	if (tree1->Empty()) cout << "Дерево пустое." << endl;
 	else tree1->Show(); // обход дерева
+	Data elem = { 3, "left" };
+	if (tree1->Search(elem)) cout << "Элемент найден" << endl;
+	else cout << "Элемент не найден" << endl;
+		cout << "Удалим элемент" << endl;
+	if (tree1->Delete(elem)) cout << "Элемент удален" << endl;
+	else cout << "Элемент не найден" << endl;
+	if (tree1->Empty()) cout << "Дерево пустое." << endl;
+	else tree1->Show(); // обход дерева
+	elem = { 11, "right" };
+	if (tree1->Delete(elem)) cout << "Элемент удален" << endl;
+	else cout << "Элемент не найден" << endl;
+	if (tree1->Empty()) cout << "Дерево пустое." << endl;
+	else tree1->Show(); // обход дерева
+	//Data o_elem = { 10, "left" };
+	//Data n_elem = { 8, "left" };  
+	// не работает
+	/*if (tree1->Update(n_elem, o_elem))
+	{
+		cout << "Элемент обновлен\n";
+		tree1->Show();
+	}
+	else cout << "Элемент не найден\n";*/
 	return 0;
 }
