816e727fb6792075513b37413e4151cab51567e7
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-09-01 10:49:17 +0000

    Completed FastDegree

---
+++ prog_c++/FastDegree.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int f(int x, int n)
+{
+	int res = 1;
+	while (n)
+	{
+		if (n == 0) return 1;
+		if (n != 0 && n % 2 == 0)
+		{
+			res *= x/2 * x;
+		}
+		if (n != 0 && n % 2 != 0)
+		{
+			res *= x;
+		}
+		--n;
+	}
+	return res;
+}
+int main()
+{
+	int x;
+	int n;
+	cin >> x;
+	cin >> n;
+	cout << f(x, n);
+	return 0;
+}
