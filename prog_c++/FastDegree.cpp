d0733073bb835a840cd83d3aa7aa6c2162812fa2
Dmitrie Tretiakov <d-tretyakov-artim2012@yandex.ru>
2020-09-02 14:25:17 +0300

    Update FastDegree.cpp

--- prog_c++/FastDegree.cpp
+++ prog_c++/FastDegree.cpp
@@ -4,19 +4,22 @@ using namespace std;
 int f(int x, int n)
 {
 	int res = 1;
-	while (n)
+	if (n == 0)
 	{
-		if (n == 0) return 1;
-		if (n != 0 && n % 2 == 0) res *= x/2 * x;
-		if (n != 0 && n % 2 != 0) res *= x;
-		--n;
+		return 1;
+	}
+	else {
+		while (n)
+		{
+			res *= x;
+			--n;
+		}
 	}
 	return res;
 }
 int main()
 {
-	int x;
-	int n;
+	int x, n;
 	cin >> x;
 	cin >> n;
 	cout << f(x, n);
