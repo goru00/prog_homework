4c4c02f0dfd5f018f78025e84c0a43699937a4e5
goru00 <d-tretyakov-artim2012@yandex.ru>
2020-09-07 20:33:10 +0300

    Completed TaylorSeries.cpp

---
+++ prog_c++/TaylorSeries.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+double f(double x, double eps)
+{
+	double sum = 0, a = 1;
+	int n = 1;
+	while (abs(a) >= eps)
+	{
+		sum += a;
+		a *= x / n;
+		n++;
+	}
+	return sum;
+}
+int main()
+{
+	double x, eps;
+	cin >> x;
+	cin >> eps;
+	cout << f(x, eps) << "\t" << exp(x) << endl;
+	return 0;
+}
