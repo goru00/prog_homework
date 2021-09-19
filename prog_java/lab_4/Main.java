eb36c157a7710d1ff52903827d899af713c01bc8
goru00 <d-tretyakov-artim2012@yandex.ru>
2021-09-19 15:35:44 +0300

    Update lab_4 Java

--- prog_java/lab_4/Main.java
+++ prog_java/lab_4/Main.java
@@ -14,14 +14,17 @@ public class Main {
             n++;
             a *= (x * x) / (2.0 * n + 1) / (2.0 * n + 2.0) * (-1.0);
         }
+        System.out.println("Кол-во итераций = " + n);
         return x * sum;
     }
 
     public static void main(String[] args) {
-        final double eps = 0.0000001;
+        final double eps = 1.0e-16;
         Scanner in = new Scanner(System.in);
         System.out.print("Введите значение x: ");
         double x = in.nextDouble();
-        System.out.println("F(x) = " + F(x, eps) + "\nf(x) = " + (2.0 / x) * Math.pow(Math.sin(x / 2.0), 2));
+        double res = F(x, eps);
+        System.out.format("F(x) = %2.16f\n", res);
+        System.out.format("f(x) = %2.16f\n", (2.0 / x) * Math.pow(Math.sin(x / 2.0), 2));
     }
 }
