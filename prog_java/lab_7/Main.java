254c7951c0b1db86a641c9adf045d3234aab4b46
goru00 <goru00@vk.com>
2021-12-04 21:04:55 +0300

    created new lab_7

--- prog_java/lab_7/Main.java
+++ prog_java/lab_7/Main.java
@@ -1,90 +1,170 @@
 package com.company;
-import java.util.ArrayDeque;
 
-class Parking<T>
+import java.util.*;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.Semaphore;
+
+// класс газеты
+class Paper
 {
-    private ArrayDeque<T> lots;
-    private int sizeLots;
+    private Semaphore sem;
+    private String article;
+    private String namePaper;
+    private String description;
+    private int price;
 
-    Parking()
+    public String getArticle() { return article; }
+    public String getNamePaper() { return  namePaper; }
+    public String getDescription() { return description; }
+    public int getPrice() { return price; }
+
+    Paper()
     {
-        sizeLots = 0;
+        this.article = null;
+        this.namePaper = null;
+        this.description = null;
     }
-    Parking(int sizeLots)
+    Paper(String article, String namePaper, String description, int price)
     {
-        this.sizeLots = sizeLots;
-        this.lots = new ArrayDeque<>();
+        this.article = article;
+        this.namePaper = namePaper;
+        this.description = description;
+        this.price = price;
     }
-
-    public boolean isLots() { return sizeLots > lots.size(); }
-
-    public void checkParking(T arg)
-    {
-        if (sizeLots > lots.size())
-        {
-            long restTime = (long) (Math.random() * 2000);
-            try
-            {
-                lots.addLast(arg);
-                System.out.println("Машина " + arg + " заняла парковочное место!");
-                Thread.sleep(restTime);
-            }
-            catch (InterruptedException e)
-            {
-
-            }
-            lots.removeLast();
-            System.out.println("Машина " + arg + " покинула стоянку. Машина простояла примерно " + ((double) restTime / (double) 1000) + " секунд.");
-        }
-    }
-}
-
-class Car extends Thread implements Runnable
-{
-    private int numCar;
-    Parking carPark;
-
-    Car()
-    {
-        this.numCar = 0;
-    }
-    Car(Parking carPark, int numCar)
-    {
-        this.carPark = carPark;
-        this.numCar = numCar;
-    }
-
-    @Override
-    public void run() {
-        long restTime = (long) (Math.random() * 2000);
-        if (carPark.isLots()) {
-            carPark.checkParking(this);
-        } else {
-            try {
-                System.out.println("Машина " + numCar + " ожидает свободного места.");
-                Thread.sleep(restTime);
-                System.out.println("Машина " + numCar + " проверяет наличие свободного парковочного места.");
-                carPark.checkParking(this);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     @Override
     public String toString()
     {
-        return "" + numCar;
+        return article + " | " + namePaper + " | " + description + " | " + price + "\n";
     }
 }
-public class Main
+
+// класс газетного киоска
+class NewsStand<T> extends Thread
 {
-    public static void main(String[] args)
+    private Semaphore sem;
+    private ArrayList<T> listPapers; // "склад" газет
+    private boolean isOpen;
+
+    public boolean getIsOpen() { return isOpen; }
+    public Semaphore getSem() { return sem; }
+    public ArrayList<T> getListPaper() { return listPapers; }
+
+    NewsStand()
     {
-        long startTime = System.currentTimeMillis();
-        Parking<Car> parking = new Parking<Car>(10);
-        for (int i = 0; i < 25; i++) {
-             new Car(parking, i + 1).start();
+        listPapers = new ArrayList<T>();
+        isOpen = true;
+    }
+    public void addPaper(T item)
+    {
+        listPapers.add(item);
+    }
+    public void getInfo()
+    {
+
+    }
+    @Override
+    public void run()
+    {
+        sem = new Semaphore(1, true);
+        while (isOpen) {
+            if (listPapers.size() == 0) isOpen = false;
+            this.interrupt();
+        }
+    }
+}
+
+// класс покупателя
+class Customer<T> extends Thread
+{
+    private int idCustomer;
+    private boolean onDeque;
+    private NewsStand NS;
+    private Semaphore sem;
+    private T data;
+
+    public int getIdCustomer() { return idCustomer; }
+
+    Customer(int idCustomer, NewsStand NS, T data)
+    {
+        this.idCustomer = idCustomer;
+        this.NS = NS;
+        this.sem = NS.getSem();
+        this.data = data;
+        onDeque = false;
+    }
+
+    @Override
+    public void run()
+    {
+        try {
+            checkDeque();
+            sem.acquire(1);
+            putPaper();
+            sem.release();
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    void putPaper()
+    {
+        if (!NS.getIsOpen()) return;
+        int rnd = new Random().nextInt(2000);
+        try {
+            sleep(rnd);
+            if (NS.getListPaper().contains(data)) {
+                System.out.println("CustomerId: " + idCustomer + " взял газету: " + data);
+                NS.getListPaper().remove(data);
+            } else {
+                System.out.println("CustomerId: " + idCustomer + " не смог взять газету(" + data + "), тк её не было");
+            }
+        } catch (InterruptedException e) {
+
+        }
+        onDeque = false;
+    }
+
+    public void checkDeque()
+    {
+        if (onDeque) return;
+        System.out.println("CustomerId: " + idCustomer + " встал в очередь");
+        onDeque = true;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        final int sizePapers = 10;
+        Paper papers[] = {
+                new Paper("199933", "Аргументы и факты", "www.aif.ru", 100),
+                new Paper("379975", "Аргументы недели", "www.argumenti.ru", 75),
+                new Paper("970680", "Ведомости", "www.vedomosti.ru", 80),
+                new Paper("942551", "Вечерняя Москва", "www.vm.ru", 55),
+                new Paper("301011", "Гудок", "www.gudok.ru", 45),
+                new Paper("442051", "Ежедневные новости. Подмосковье", "www.mosregtoday.ru", 24),
+                new Paper("334406", "Завтра", "www.zavtra.ru", 12),
+                new Paper("467285", "Известия", "www.izvestia.ru", 35),
+                new Paper("417807", "Коммерсант", "www.kommersant.ru", 30),
+                new Paper("881994", "Комсомольская правда", "www.kp.ru", 75)
+        };
+        NewsStand<Paper> NS = new NewsStand<>();
+        for (int i = 0; i < 5; i++) {
+            int random = 0 + (int) (Math.random() * sizePapers);
+            NS.addPaper(papers[random]);
+        }
+        try {
+            NS.start();
+            int i = 1;
+            while(NS.getIsOpen()) {
+                Customer c = new Customer(i, NS, papers[0 + (int)(Math.random() * sizePapers)]);
+                c.start();
+                i++;
+                Thread.sleep(100);
+            }
+            NS.interrupt();
+        } catch (Exception e) {
+
         }
     }
 }
