caebda5a0c6f2250b5232354bdd6a1cefa65ba94
goru00 <goru00@vk.com>
2022-04-04 22:53:40 +0300

    fixed

---
+++ prog_web/web4/src/components/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    width: 550,
+    height: 733,
+    size: "stretch",
+    minWidth: 315,
+    maxWidth: 550,
+    minHeight: 420,
+    maxHeight: 733,
+    maxShadowOpacity: 0.5,
+    showCover: false,
+    mobileScrollSupport: false
+};
