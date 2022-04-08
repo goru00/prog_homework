29cfaea83bb60746471ac5438541f9412a172a1a
goru00 <goru00@vk.com>
2022-04-08 21:56:27 +0300

    completed hash_function

--- prog_c++/HashFunction.cpp
+++ prog_c++/HashFunction.cpp
@@ -1,66 +1,54 @@
 #include<iostream>
 #include<string>
 
-class List 
+template<class T>
+class List
 {
 private:
+    template<class T>
     class Node
     {
     public:
-        int key;
-        std::string val;
-        Node * next;
-        Node(int key, std::string val) {
-            this->key = key;
-            this->val = val;
-            this->next = nullptr;
+        T data;
+        Node* next;
+        Node(T data = T(), Node* next = nullptr) {
+            this->data = data;
+            this->next = next;
         }
-        Node(Node * n) {
-            this->key = n->key;
-            this->val = n->val;
-            this->next = nullptr;
+        Node(Node* n, Node* next = nullptr) {
+            this->data = n->data;
+            this->next = next;
         }
     };
-    Node * head;
-    int size; 
+    Node<T>* head;
+    int size;
 public:
     List() {
         head = nullptr;
         size = 0;
     }
-    void pushBack(Node * n) {
+    void pushBack(T data) {
         if (head == nullptr) {
-            head = new Node(n);
-            return;
-        } else {
-            Node * curr = head;
+            head = new Node<T>(data);
+        }
+        else {
+            Node<T>* curr = head;
             while (curr->next != nullptr) {
                 curr = curr->next;
             }
-            curr->next = n;
+            curr->next = new Node<T>(data);
         }
-    }
-    void pushBack(int key, std::string val) {
-        if (head == nullptr) {
-            head = new Node(key, val);
-            return;
-        } else {
-            Node * curr = head;
-            while (curr->next != nullptr) {
-                curr = curr->next;
-            }
-            curr->next = new Node(key, val);
-        }
-    }
-    void show() {
-        if (isEmpty()) return;
-        Node * curr = head;
-        while (curr) {
-            std::cout << "Ключ: " << curr->key << " " << curr->val << std::endl;
-            curr = curr->next;
-        }
+        ++size;
     }
     bool isEmpty() { return head == nullptr; }
+    friend std::ostream& operator<<(std::ostream& out, const List<T>& arg) {
+        Node<T>* curr = arg.head;
+        while (curr) {
+            out << "-->" << curr->data;
+            curr = curr->next;
+        }
+        return out;
+    }
 };
 
 class Data {
@@ -72,8 +60,8 @@ private:
 public:
     Data() {
         this->id = 0;
-        this->name = nullptr;
-        this->direction = nullptr;
+        this->name = "";
+        this->direction = "";
         this->price = 0;
     }
     Data(int id, std::string name, std::string direction, int price) {
@@ -82,16 +70,78 @@ public:
         this->direction = direction;
         this->price = price;
     }
+    friend int operator%(const Data& arg, int val) {
+        if (val == 0) {
+            throw std::runtime_error("zero division error");
+        }
+        return arg.id % val;
+    }
     friend std::ostream& operator<<(std::ostream& out, const Data& arg) {
-        out << arg.id << " " << arg.name << " " << arg.direction << " " << arg.price << std::endl;
+        out << "[" << arg.id << " " << arg.name << " " << arg.direction << " " << arg.price << "]";
         return out;
     }
 };
 
+template<class T>
+class Hash
+{
+private:
+    int K;
+    List<T>* blocks;
+    int checkPrime(int n) {
+        if (n == 1 || n == 0) {
+            return 0;
+        }
+        for (int i = 2; i < n / 2; i++) {
+            if (n % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    int getPrime(int n) {
+        if (n % 2 == 0) {
+            n++;
+        }
+        while (!checkPrime(n)) {
+            n = n + 2;
+        }
+        return n;
+    }
+    int hashFunction(T key) {
+        return key % K;
+    }
+public:
+    Hash(int size) {
+        this->K = getPrime(size);
+        blocks = new List<T>[K];
+    }
+    void insert(T data) {
+        int index = hashFunction(data);
+        blocks[index].pushBack(data);
+    }
+    void show() {
+        for (int index = 0; index < K; index++) {
+            std::cout << "ячейка[" << index << "]" << blocks[index] << std::endl;
+        }
+    }
+};
+
 int main() {
-    List * list = new List();
-    list->pushBack(10, "gjerigjioerjogr");
-    list->pushBack(12, "gerjgioerjiogjioergoer");
-    list->show();
+    setlocale(0, "");
+    const int N = 6;
+    Data mass[] = {
+        Data(2345, "Иванов Сергей Степанович", "Разнорабочий", 28000),
+        Data(2346, "Краснов Сергей Георгиевич", "Сантехник", 32000),
+        Data(2347, "Бендер Остап Ибрагимович", "Директор", 62000),
+        Data(2349, "Петров Сергей Степанович", "Электрик", 36000),
+        Data(2356, "Кирпичев Владимир Викторович", "Начальник смены", 48000),
+        Data(2378, "Абрамов Иван Петрович", "Электрик", 36000)
+    };
+    Hash<Data> table(N);
+    for (int index = 0; index < N; index++) {
+        table.insert(mass[index]);
+    }
+    table.show();
     return 0;
 }
